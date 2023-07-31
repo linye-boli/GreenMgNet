cf713745917ee075da868f1aaa069e8882d02e4c
linye-boli <linye@changchunboli.com>
2023-07-31 07:32:01 +0000

    add device

--- scripts/lno2d_ml_1.sh
+++ scripts/lno2d_ml_1.sh
@@ -2,11 +2,11 @@ for i in 2 3
 do
     for ds in darcy invdist
     do
-        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel '-1' --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025
-        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 0 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025
-        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 1 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025
-        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 2 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025
-        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 3 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025
+        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel '-1' --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025 --device 1
+        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 0 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025 --device 1
+        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 1 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025 --device 1
+        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 2 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025 --device 1
+        python lno/lno_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 3 --seed $i --batch_size 8 --ntest 100 --epochs 200 --lr 0.00025 --device 1
     done 
 done
 
