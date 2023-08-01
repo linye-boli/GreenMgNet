3b110673b4e2bbefd85028de0e69cba19c5c9008
linye-boli <linye@changchunboli.com>
2023-08-01 06:20:27 +0000

    modify ft2d ml

--- scripts/ft2d_ml_1.sh
+++ scripts/ft2d_ml_1.sh
@@ -2,11 +2,11 @@ for i in 2 3
 do
     for ds in darcy invdist
     do
-        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel '-1' --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 1
-        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 0 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 1
-        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 1 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 1
-        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 2 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 1
-        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 3 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 1
+        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel '-1' --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 5
+        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 0 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 5
+        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 1 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 5
+        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 2 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 5
+        python ft/ft_2d.py --dataset_nm $ds --trasub 5 --testsub 5 --clevel 0 --mlevel 3 --seed $i --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --log_dir /home/omnisky/linye/research/MINO/logs --cfg_path /home/omnisky/linye/research/MINO/cfgs --dataset_path /home/omnisky/linye/research/pde_data --device 5
     done 
 done
 
