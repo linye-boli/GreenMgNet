7229f2e34739a4df7b8ad8eea17b0c1d87b88ff0
linye-boli <linye@changchunboli.com>
2023-09-11 06:02:47 +0000

    v100 result 1d

--- scripts/ft1d.sh
+++ scripts/ft1d.sh
@@ -2,7 +2,7 @@ for s in 16 8 4 2 1
 do
     for ds in lnabs cosine burgers poisson
     do
-        for c in 0 1 2 3 4
+        for c in 1 2 3 4
         do 
             for m in '-1' 0 1 2 3 4
             do
