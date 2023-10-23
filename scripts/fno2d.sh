08ea2ceb98478a7ba65acbf7ae3531bd39ddb959
linye-boli <linye@changchunboli.com>
2023-10-23 14:55:34 +0000

    V100 result

--- scripts/fno2d.sh
+++ scripts/fno2d.sh
@@ -2,12 +2,33 @@ for s in 1 2 3 4 5
 do
     for ds in darcy invdist
     do
-        for c in 0 1 2 3 4
-        do 
-            for m in '-1' 0 1 2 3 4
-            do
-                python fno/fourier_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 16 --ntest 100 --device $1
-            done 
-        done
+        case $s in
+            1)
+            for c in 2 3 4
+            do 
+                for m in '-1' 0 1 2 3 4
+                do
+                    python fno/fourier_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 16 --ntest 100 --device $1
+                done         
+            done
+            ;;
+            2)
+            for c in 0 1 2 3 4
+            do 
+                for m in '-1' 0 1 2 3 4
+                do
+                    python fno/fourier_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 16 --ntest 100 --device $1
+                done         
+            done
+            ;;
+            3 | 4 | 5)
+            for c in 0 1 2 3 4
+            do 
+                for m in '-1' 0 1 2 3 4
+                do
+                    python fno/fourier_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 16 --ntest 100 --device $1
+                done         
+            done
+        esac
     done 
 done 
