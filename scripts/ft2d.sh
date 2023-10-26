e125adcd395a866fb6174a23209d85980bdc95db
linye-boli <linye@changchunboli.com>
2023-10-26 03:54:08 +0000

    modify ft2d.sh

--- scripts/ft2d.sh
+++ scripts/ft2d.sh
@@ -2,33 +2,12 @@ for ds in darcy invdist
 do
     for s in 5 3 2
     do
-        case $s in 
-            2) 
-            for c in 0 1 2 3
+        for c in 0 1 2 3
+        do
+            for m in '-1' 0 1 2 3
             do
-                for m in '-1' 0 1 2 3
-                do
-                    python ft/ft_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --device $1
-                done 
-            done
-            ;;
-            3)
-            for c in 1 2 3
-            do
-                for m in '-1' 0 1 2 3
-                do
-                    python ft/ft_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --device $1
-                done 
-            done
-            ;;
-            5)
-            for c in 0 1 2 3
-            do
-                for m in '-1' 0 1 2 3
-                do
-                    python ft/ft_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --device $1
-                done 
-            done
-            esac
+                python ft/ft_2d.py --dataset_nm $ds --trasub $s --testsub $s --clevel $c --mlevel $m --seed $2 --batch_size 2 --ntest 100 --epochs 200 --lr 0.00025 --device $1
+            done 
+        done
     done 
 done 
