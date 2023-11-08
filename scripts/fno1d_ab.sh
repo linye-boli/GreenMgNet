8f46c1053e9c9f79423b796440a9cf23f10b1574
linye-boli <linye@changchunboli.com>
2023-11-08 08:36:17 +0000

    add cm ablation study scripts

---
+++ scripts/fno1d_ab.sh
@@ -0,0 +1,9 @@
+for ab in ca cb cc cd ce
+do
+    python fno/fourier_1d_ab.py --dataset_nm 'burgers' --ab_cfg $ab --trasub 4 --testsub 4 --seed $2 --batch_size 16 --ntest 200 --device $1
+done 
+
+for ab in ma mb mc md me
+do
+    python fno/fourier_1d_ab.py --dataset_nm 'burgers' --ab_cfg $ab --trasub 4 --testsub 4 --seed $2 --batch_size 16 --ntest 200 --device $1
+done 
