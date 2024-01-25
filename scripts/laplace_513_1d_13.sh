0d4eb8b76ae6fda6423be06b0f0b475e31611ee8
lyokka <linye@changchunboli.com>
2024-01-25 01:41:30 +0000

    A6000 result

--- scripts/laplace_513_1d_13.sh
+++ scripts/laplace_513_1d_13.sh
@@ -3,7 +3,7 @@ ep=1500
 
 for s in 1 3
 do
-    for task in laplace
+    for task in laplace schrodinger
     do
         # baseline fno
         python fourier_1d.py --device 1 --task $task --seed $s --lr_adam 1e-3 --ep_adam 500 --sch --res $res
@@ -24,7 +24,7 @@ do
 
                 for k in 5 3 1
                 do
-                    for m in 0 1 3 5 7
+                    for m in 0 1 3 5 7 15
                     do
                         # dd green mg
                         python green_mgnet.py --device 1 --task $task --act $act --k $k --m $m --seed $s --mode dd_mg --lr_adam 1e-3 --ep_adam $ep --sch --h $h --res $res
