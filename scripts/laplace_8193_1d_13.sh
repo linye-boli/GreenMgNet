f55f4127b6785549adbd133d6fd30fb17c2c0220
lyokka <linye@changchunboli.com>
2024-01-28 06:21:17 +0000

    A6000 result

--- scripts/laplace_8193_1d_13.sh
+++ scripts/laplace_8193_1d_13.sh
@@ -22,9 +22,9 @@ do
                 #     python green_mgnet.py --device 1 --task $task --act $act --r $r --seed $s --mode lr_gl --lr_adam 1e-3 --ep_adam 1000 --sch --h $h --res 2049
                 # done
 
-                for k in 7 5 #3 1
+                for k in 5 3 # 1
                 do
-                    for m in 0 1 3 7 15 31 63 127 255
+                    for m in 0 1 3 7 15 31 # 63 127 255
                     do
                         # dd green mg
                         python green_mgnet.py --device 1 --task $task --act $act --k $k --m $m --seed $s --mode dd_mg --lr_adam 1e-3 --ep_adam $ep --sch --h $h --res $res
