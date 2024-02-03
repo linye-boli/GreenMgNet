c2ffe49c3e1c0160df79677643677de9f82b3599
lyokka <linye@changchunboli.com>
2024-02-03 02:40:20 +0000

    invdist 256

--- scripts/invdist_129_2d_024.sh
+++ scripts/invdist_129_2d_024.sh
@@ -1,4 +1,4 @@
-res=129
+res=257
 ep=1500
 
 for s in 0 2 4
@@ -22,7 +22,7 @@ do
                 #     python green_mgnet.py --device 1 --task $task --act $act --r $r --seed $s --mode lr_gl --lr_adam 1e-3 --ep_adam 1000 --sch --h $h --res 2049
                 # done
 
-                for k in 5 3 1
+                for k in 7 5 3 1
                 do
                     for m in 0 1 3 5 7 15
                     do
