d668fd0d54980ff060148ec2f6f40d2dcd7d672e
linye-boli <linye@changchunboli.com>
2024-03-27 13:17:17 +0800

    modify dd_gmg scripts

--- scripts/dd_gmg_1d.sh
+++ scripts/dd_gmg_1d.sh
@@ -2,17 +2,17 @@ for s in 0 1 2 3 4
 do
     for task in expdecay poisson doublesingularity
     do
-        for act in rational 
+        for act in relu 
         do
             for h in 64
             do
-                for n in 10 #15 14 13 12
+                for n in 12 #15 14 13 12
                 do
-                    for k in 3 5 7 
+                    for k in 3 5 7
                     do
-                        for m in 5 3 1 0 31 15 7
+                        for m in 15 7 5 3 1 0
                         do
-                            python dd_gmg_1d.py --device 3 --task $task --act $act --seed $s --ep_adam 1000 --k $k --m $m --h $h --n $n --bsz 8
+                            python dd_gmg_1d.py --device 0 --task $task --act $act --seed $s --ep_adam 1000 --k $k --m $m --h $h --n $n --bsz 8
                         done 
                     done 
                 done 
--- scripts/green_1d.sh
+++ scripts/green_1d.sh
@@ -2,13 +2,13 @@ for s in 0 1 2 3 4
 do
     for task in expdecay poisson doublesingularity cosine logarithm
     do
-        for act in rational
+        for act in relu
         do
             for h in 64
             do
-                for n in 10
+                for n in 12
                 do
-                    python green_1d.py --device 2 --task $task --act $act --seed $s --ep_adam 1000 --h $h --n $n --bsz 8
+                    python green_1d.py --device 1 --task $task --act $act --seed $s --ep_adam 1000 --h $h --n $n --bsz 8
                 done
             done
         done
