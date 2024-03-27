de1488917663d032dfec07394b15c37c68183afa
linye-boli <linye@changchunboli.com>
2024-03-27 13:58:34 +0800

    modify scripts

--- scripts/dd_gmg_2d.sh
+++ scripts/dd_gmg_2d.sh
@@ -1,18 +1,18 @@
-for s in 0 1 2
+for s in 0 1 2 3 4
 do
     for task in poisson
     do
-        for act in tanh rational 
+        for act in relu
         do
             for h in 64
             do
                 for n in 6
                 do
-                    for k in 1 2 3
+                    for k in 3 2 1
                     do
-                        for m in 7 5 3 1 0
+                        for m in 15 7 3 1 0
                         do
-                            python dd_gmg_1d.py --device 1 --task $task --act $act --seed $s --ep_adam 1000 --k $k --m $m --h $h --n $n --bsz 4
+                            python dd_gmg_2d.py --device 1 --task $task --act $act --seed $s --ep_adam 1000 --k $k --m $m --h $h --n $n --bsz 4
                         done 
                     done 
                 done 
---
+++ scripts/green_2d.sh
@@ -0,0 +1,16 @@
+for s in 0 1 2 3 4
+do
+    for task in poisson invdist
+    do
+        for act in relu
+        do
+            for h in 64
+            do
+                for n in 6
+                do
+                    python green_2d.py --device 0 --task $task --act $act --seed $s --ep_adam 1000 --h $h --n $n --bsz 4
+                done
+            done
+        done
+    done
+done
