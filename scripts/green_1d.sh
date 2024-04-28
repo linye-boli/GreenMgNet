126e48e42ddb156f53f6f837325a0a02764d7903
linye-boli <linye@changchunboli.com>
2024-04-28 14:04:19 +0800

    V100 result

--- scripts/green_1d.sh
+++ scripts/green_1d.sh
@@ -1,16 +1,16 @@
-for s in 0
+for s in 0 1 2 3 4 5
 do
     for act in rational relu
     do
-        for task in poisson cosine logarithm expdecay doubleholes
+        for task in poisson cosine logarithm
         do
             for h in 50
             do
                 for n in 9
                 do
-                    for p in 0.01 0.03 0.05 0.07 0.1 0.15 0.25 0.30 0.4 0.5 0.7 0.9 1.0
+                    for p in 1.0 0.01 0.03 0.05 0.07 0.1 0.15 0.25 0.30 0.4 0.5 0.7 0.9 
                     do
-                        python green_1d.py --device 0 --task $task --act $act --seed $s --ep_adam 2500 --h $h --p $p --n $n --bsz 20 --sch
+                        python green_1d.py --device 0 --task $task --act $act --seed $s --ep_adam 1000 --h $h --p $p --n $n --bsz 100 --sch
                     done
                 done
             done
